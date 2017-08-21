15e696cd224b450ef85f26255068efa20001db87
jefferson otoni lima <jeff.otoni@gmail.com>
2017-08-21 08:46:52 -0300

    correction documentation example main

--- example/main.go
+++ example/main.go
@@ -1,3 +1,18 @@
+/*
+* Go Library (C) 2017 Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+* @project     Ukkbox
+* @package     main
+* @author      @jeffotoni
+* @size        21/08/2017
+*
+ */
+
 package main
 
 import (
@@ -22,17 +37,17 @@ func main() {
 	//
 	c := new(Collor)
 	c.Cor = "green"
-	c.Cprintln("Start port")
+	c.Cprintln("Color Green")
 
 	//
 	// Third way to instantiate
 	//
 	red := Collor{"red"}
-	red.Cprintln("Start port red")
+	red.Cprintln("Color red")
 
 	//
 	// Fourth way to instantiate
 	//
-	Red.Cprintln("Collor red2")
+	Red.Cprintln("Red color instance otherwise")
 
 }
