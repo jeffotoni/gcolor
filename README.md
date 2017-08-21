9cac2207acb06cebc10777356aeeb79164a348a7
jefferson otoni lima <jeff.otoni@gmail.com>
2017-08-21 08:35:54 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # gocolor
+
 A simple lib to help you color when developing some bash application using ANSI-COLORS.
+
+The goal is to make the lib leaner and leaner.
+
+The gocollor was made only with this intúito of leaving its characters in the colored bash.
+
+
+## Install
+
+```go
+
+go get github.com/jeffotoni/gocollor
+
+```
+
+## Use gocollor
+
+```go
+
+package main
+
+import (
+	// "fmt"
+	. "github.com/jeffotoni/gocollor"
+)
+
+//
+// start
+//
+func main() {
+
+	//
+	// First instantiate form
+	//
+	var color Collor
+	color.Cor = "purple"
+	color.Cprintln("Purple")
+
+	//
+	// Second way to instantiate
+	//
+	c := new(Collor)
+	c.Cor = "green"
+	c.Cprintln("Start port")
+
+	//
+	// Third way to instantiate
+	//
+	red := Collor{"red"}
+	red.Cprintln("Start port red")
+
+	//
+	// Fourth way to instantiate
+	//
+	Red.Cprintln("Collor red2")
+
+}
+
+```
---
+++ collor.go
@@ -0,0 +1,97 @@
+/*
+* Ukkbox Go Library (C) 2017 Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+* @project     Ukkbox
+* @package     main
+* @author      @jeffotoni
+* @size        01/06/2017
+*
+ */
+
+package gocollor
+
+import (
+	"fmt"
+	"strings"
+)
+
+//
+// Color map
+//
+var MapCor map[string]string
+
+//
+// Color Struct
+//
+type Collor struct {
+	Cor string
+}
+
+//
+// version Generic
+//
+type CorGeneric struct {
+	Cor string
+}
+
+//
+// Implementing the color map
+//
+func MapCollor() map[string]string {
+
+	MapCor = make(map[string]string)
+
+	MapCor["black"] = "\033[0;30m#\033[0m"
+	MapCor["red"] = "\033[0;31m#\033[0m"
+	MapCor["green"] = "\033[0;32m#\033[0m"
+	MapCor["yellow"] = "\033[0;33m#\033[0m"
+	MapCor["blue"] = "\033[0;34m#\033[0m"
+	MapCor["purple"] = "\033[0;35m#\033[0m"
+	MapCor["cyan"] = "\033[0;36m#\033[0m"
+	MapCor["white"] = "\033[37m#\033[0m"
+
+	return MapCor
+}
+
+func (c Collor) MapColor() string {
+
+	MapCor := MapCollor()
+
+	return MapCor[c.Cor]
+}
+
+func (c Collor) Cprintln(text string) {
+
+	cornow := c.MapColor()
+	corSplit := strings.Split(cornow, "#")
+	stringt := corSplit[0] + text + corSplit[1]
+	fmt.Println(stringt)
+}
+
+func (c CorGeneric) MapColor() string {
+
+	MapCor := MapCollor()
+
+	return MapCor[c.Cor]
+}
+
+func (c CorGeneric) Cprintln(text string) {
+
+	cornow := c.MapColor()
+	corSplit := strings.Split(cornow, "#")
+	stringt := corSplit[0] + text + corSplit[1]
+	fmt.Println(stringt)
+}
+
+var Yellow CorGeneric = CorGeneric{"yellow"}
+var Black CorGeneric = CorGeneric{"black"}
+var Green CorGeneric = CorGeneric{"green"}
+var Blue CorGeneric = CorGeneric{"blue"}
+var Purple CorGeneric = CorGeneric{"purple"}
+var Cyan CorGeneric = CorGeneric{"cyan"}
+var Red CorGeneric = CorGeneric{"red"}
